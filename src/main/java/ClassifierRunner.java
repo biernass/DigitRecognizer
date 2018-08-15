a3c5569d5b5cbd80dd72fc766c2196477809847e
Michał Biernacki <micbiernacki@gmail.com>
2018-08-15 10:45:47 +0200

    parsing String to int. Create RecordsList

--- src/main/java/ClassifierRunner.java
+++ src/main/java/ClassifierRunner.java
@@ -11,32 +11,36 @@ public class ClassifierRunner {
 
     public static void main(String[] args) {
 
-        List<String> trainingsampleList = new ArrayList<>();
+
+        List<String> lines = new ArrayList<>();
 
 
         try {
-            Path pathTrainingsample = Paths.get("trainingsample.csv");
-            trainingsampleList.addAll(Files.readAllLines(pathTrainingsample));
-        }
-
-        catch (IOException e) {
+            Path pathOfTrainingsample = Paths.get("trainingsample.csv");
+            lines.addAll(Files.readAllLines(pathOfTrainingsample));
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
-        List<String[]> trainingsampleListAsArraysWithoutLabels = trainingsampleList
+        List<String[]> trainingsampleListAsStringsArraysWithoutLabels = lines
                 .stream()
-                .map(x->x.split(","))
+                .map(x -> x.split(","))
                 .collect(Collectors.toList());
-        trainingsampleListAsArraysWithoutLabels.remove(0);
+        trainingsampleListAsStringsArraysWithoutLabels.remove(0);
 
-        List<List<Integer>> trainingsampleListAsIntegers = trainingsampleListAsArraysWithoutLabels
-                .stream()
-                .map(x->{
-                    return Arrays.stream(x)
-                            .map(y->Integer.parseInt(String.valueOf(y)))
-                            .collect(Collectors.toList());
+        List<Record> RecordsList = trainingsampleListAsStringsArraysWithoutLabels.stream()
+                .map(x -> Arrays.stream(x)
+                        .map(y -> Integer.parseInt(y))
+                        .collect(Collectors.toList()))      //parsing String to int
+                .map(x -> {
+                    Record record = new Record();
+                    record.Number = x.get(0);
+                    record.Pixels = x.subList(1, x.size()).toArray(new Integer[0]);
+                    return record;
                 })
+
                 .collect(Collectors.toList());
+
 
         System.out.println("hi");
     }
---
+++ src/main/java/Record.java
@@ -0,0 +1,6 @@
+public class Record {
+
+    public Integer Number;
+    public Integer[] Pixels;
+
+}
